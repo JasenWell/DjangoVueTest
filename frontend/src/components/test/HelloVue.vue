4a9f68e63e3018376c097930d2784869eb490b5c
hjh200507609 <409762605@qq.com>
2019-12-09 17:31:47 +0800

    Update HelloVue.vue

--- frontend/src/components/test/HelloVue.vue
+++ frontend/src/components/test/HelloVue.vue
@@ -1,9 +1,26 @@
 <template>
   <div id="vue">
     Hello vue.js {{msg}}
-    <el-button>默认按钮</el-button>
-    <el-button type="primary">主要按钮</el-button>
-    <el-button type="text">文字按钮</el-button>
+
+
+    <br>
+    <el-select v-model="value" placeholder="请选择">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+      </el-option>
+    </el-select>
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+      <el-button type="text">文字按钮</el-button>
+    </el-row>
   </div>
 </template>
 
@@ -12,7 +29,24 @@
     name: "HelloVue",
     data (){ //注意：data即使不需要传数据，也必须return,否则会报错
       return {
-        msg: "啦啦啦啦啦"
+        msg: "按钮测试",
+        options: [{
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }, {
+          value: '选项4',
+          label: '龙须面'
+        }, {
+          value: '选项5',
+          label: '北京烤鸭'
+        }],
+        value: ''
       }
     }
   }
